3d482d6ddaabcd3b502fc1762885b2c759f10b2b
Andre Taveira <andresvtaveira@gmail.com>
2022-09-07 00:40:56 -0300

    Test config

--- src/main/java/com/andretaveira/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/andretaveira/helpdesk/HelpdeskApplication.java
@@ -1,50 +1,12 @@
 package com.andretaveira.helpdesk;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.andretaveira.helpdesk.domain.Call;
-import com.andretaveira.helpdesk.domain.Client;
-import com.andretaveira.helpdesk.domain.Operator;
-import com.andretaveira.helpdesk.domain.enums.Priority;
-import com.andretaveira.helpdesk.domain.enums.Profile;
-import com.andretaveira.helpdesk.domain.enums.Status;
-import com.andretaveira.helpdesk.repositories.CallRepository;
-import com.andretaveira.helpdesk.repositories.ClientRepository;
-import com.andretaveira.helpdesk.repositories.OperatorRepository;
-
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
+public class HelpdeskApplication {
 
-	@Autowired
-	private OperatorRepository operatorRepository;
-	
-	@Autowired
-	private ClientRepository clientRepository;
-
-	@Autowired
-	private CallRepository callRepository;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Operator operator = new Operator(null, "João", "74060636010", "joao@mail.com", "1234");
-		operator.addProfile(Profile.ADMIN);
-		
-		Client client = new Client(null, "Maria", "41437920012", "maria@mail.com", "1234");
-		
-		Call call = new Call(null, Priority.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", operator, client);
-		
-		operatorRepository.saveAll(Arrays.asList(operator));
-		clientRepository.saveAll(Arrays.asList(client));
-		callRepository.saveAll(Arrays.asList(call));
-	}
-
 }
---
+++ src/main/java/com/andretaveira/helpdesk/services/DBService.java
@@ -0,0 +1,43 @@
+package com.andretaveira.helpdesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andretaveira.helpdesk.domain.Call;
+import com.andretaveira.helpdesk.domain.Client;
+import com.andretaveira.helpdesk.domain.Operator;
+import com.andretaveira.helpdesk.domain.enums.Priority;
+import com.andretaveira.helpdesk.domain.enums.Profile;
+import com.andretaveira.helpdesk.domain.enums.Status;
+import com.andretaveira.helpdesk.repositories.CallRepository;
+import com.andretaveira.helpdesk.repositories.ClientRepository;
+import com.andretaveira.helpdesk.repositories.OperatorRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private OperatorRepository operatorRepository;
+	
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@Autowired
+	private CallRepository callRepository;
+
+	public void instance() {
+		Operator operator = new Operator(null, "João", "74060636010", "joao@mail.com", "1234");
+		operator.addProfile(Profile.ADMIN);
+
+		Client client = new Client(null, "Maria", "41437920012", "maria@mail.com", "1234");
+
+		Call call = new Call(null, Priority.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", operator,
+				client);
+
+		operatorRepository.saveAll(Arrays.asList(operator));
+		clientRepository.saveAll(Arrays.asList(client));
+		callRepository.saveAll(Arrays.asList(call));
+	}
+}
